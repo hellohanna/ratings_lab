9705fbd43e114aa09daf43d50854910840105448
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-01 13:03:04 -0700

    created users, movies and ratings tables. add repr in users

---
+++ model.py
@@ -0,0 +1,81 @@
+"""Models and database functions for Ratings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of ratings website."""
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return ("<User user_id={} email={}>".format(self.user_id, self.email))
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    zipcode = db.Column(db.String(15), nullable=True)
+
+
+# Put your Movie and Rating model classes here.
+
+class Movie(db.Model):
+    """Movie table"""
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return ("<Movie movie_id={} title={} IMDb {} release_date {}> "\
+            .format(self.movie_id, self.title,self.IMDb, self.release_date))
+
+    __tablename__ = "movie"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    release_date = db.Column(db.DateTime, nullable=False)
+    IMDb = db.Column(db.String(120), nullable=False)
+
+
+class Ratings_data(db.Model):
+    """Movie ratings table"""
+    __tablename__  = "ratings_data"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.movie_id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+
+
+
+##############################################################################
+# Helper functions
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
